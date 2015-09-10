e8481ee40d6af61a67810ece59963b62bea9b0e0
StephenHe <Stephen@StephenHes-MacBook-Pro.local>
2015-09-10 18:22:23 +0800

    Initial Commit

---
+++ MyProjectOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProjectOne
+//
+//  Created by StephenHe on 9/10/15.
+//  Copyright (c) 2015 StephenHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyProjectOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProjectOne
+//
+//  Created by StephenHe on 9/10/15.
+//  Copyright (c) 2015 StephenHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
